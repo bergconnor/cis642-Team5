86d6d9b9a9093a4cd150340757ef5ac01c230854
bergconnor <cberg1@ksu.edu>
2016-11-07 19:03:01 -0600

    Updated activity_main.xml and added button handlers

--- WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/MainActivity.java
+++ WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/MainActivity.java
@@ -2,6 +2,7 @@ package edu.ksu.cis.waterquality;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAboutButtonClicked(View v) {
+
+    }
+
+    public void onHelpButtonClicked(View v) {
+
+    }
+
+    public void onPictureButtonClicked(View v) {
+
+    }
+
+    public void onHistoryButtonClicked(View v) {
+
+    }
+
+    public void onSpreadsheetButtonClicked(View v) {
+
     }
 }
