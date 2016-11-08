e6fd8edfe75d18ee7b1512e22cefca8695dbcbed
bergconnor <cberg1@ksu.edu>
2016-11-07 19:16:45 -0600

    Added activity to retrieve location information

--- WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/MainActivity.java
+++ WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/MainActivity.java
@@ -1,10 +1,20 @@
 package edu.ksu.cis.waterquality;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CAMERA_REQUEST = 1571;
+    private static final int LOCATION_REQUEST = 1995;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +31,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPictureButtonClicked(View v) {
-
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file = new File(Environment.getExternalStorageDirectory()
+                + File.separator + "image.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, CAMERA_REQUEST);
     }
 
     public void onHistoryButtonClicked(View v) {
@@ -30,5 +44,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void onSpreadsheetButtonClicked(View v) {
 
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            switch (requestCode) {
+                case CAMERA_REQUEST:
+                    break;
+                case LOCATION_REQUEST:
+                    break;
+            }
+        }
     }
 }
