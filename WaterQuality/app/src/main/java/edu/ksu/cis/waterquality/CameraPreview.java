624bf434462840e2669330a4dbb2fdef94c5f752
Unknown <jckostner94@ksu.edu>
2017-04-11 09:37:00 -0500

    Started CameraPreview class

---
+++ WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/CameraPreview.java
@@ -0,0 +1,16 @@
+package edu.ksu.cis.waterquality;
+
+import android.graphics.Paint;
+import android.view.SurfaceView;
+
+/**
+ * Created by Jacob on 4/6/2017.
+ */
+
+public class CameraPreview extends SurfaceView {
+
+    private Paint paint = new Paint();
+
+    @Override
+    protected void onDraw()
+}
