f6fb3d6412e15ae06c7d294994e3214d2af041a9
jckostner94 <jckostner94@k-state.edu>
2017-04-25 10:46:36 -0500

    WIP on CameraFragment and CameraActivity : Still have black bar issue, need to implement overlay and intent back to ImageActivity

--- WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/CameraActivity.java
+++ WaterQuality/app/src/main/java/edu/ksu/cis/waterquality/CameraActivity.java
@@ -2,12 +2,15 @@ package edu.ksu.cis.waterquality;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CameraActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_camera);
         if (null == savedInstanceState) {
             getFragmentManager().beginTransaction()
